cde61090548408073e1a3637865b1bd9d2092f06
Katrina Tollin & Jessica Bohn & Jason Pelletier <pair+ATXkatrina.JessBohn.lukin41280@devbootcamp.com>
2016-08-10 17:05:23 -0500

    Create User seeds

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+ User.delete_all
+
+ # Users seeds
+ User.create(username: "ATXKatrina", password: "katsandcoco")
+ User.create(username: "lukin41280", password: "doubledadjokes")
+ User.create(username: "seanswitt", password: "levelexpert")
+ User.create(username: "kpatton", password: "newhoodieswag")
+ User.create(username: "flyingsarah", password: "aerialcoding")
+ User.create(username: "tj-oh", password: "bestguitarist")
+ User.create(username: "cjessett", password: "sandalman")
+ User.create(username: "mmmcgee", password: "punkrockqueen")
+ User.create(username: "tomscode", password: "istherealjoke")
+
+
+
