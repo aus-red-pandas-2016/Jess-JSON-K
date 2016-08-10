61016faaa558c187186479aefe184755fc0ca84d
Katrina Tollin & Jessica Bohn & Jason Pelletier <pair+ATXkatrina.JessBohn.lukin41280@devbootcamp.com>
2016-08-10 12:05:18 -0500

    Add models and migrations(non-complete)

---
+++ db/migrate/20160810114923_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.string :vote_type
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
