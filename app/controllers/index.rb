bea0101f428f74d8f1450ad67c56ae742557ca8e
Jason Pelletier <jpelletier19@me.com>
2016-08-10 17:34:59 -0500

    login view and controller complete. tests in process

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,13 @@ get '' do
   erb :index
 end
 
-get '/login' do
-
+post '/login' do
+  @user = User.find_by(params[:username])
+  if @user.password == [params[:password]]
+    session[:id] = @user.id
+    erb :index
+  else
+    alert "Incorrect Username and/or Password\nTry Again"
+    erb :login
+  end
 end
