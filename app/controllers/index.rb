6a6cc3246c5676fcef2c2ee86eda52bdf2dd22ae
Jason Pelletier <jpelletier19@me.com>
2016-08-10 18:22:04 -0500

    all merge nice up in here

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,5 @@
 get '/' do
-
   erb :index
 end
 
-get '/login' do
 
-  erb :login
-end
-
-post '/login' do
-  @user = User.find_by(username: params[:username])
-  if @user
-      @user.password == params[:password]
-      session[:id] = @user.id
-      redirect "/"
-  else
-    #put an alert here
-    redirect "/login"
-  end
-
-end
--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -12,3 +12,20 @@ post '/user/new' do
     redirect '/register'
   end
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:username])
+  if @user
+      @user.password == params[:password]
+      session[:id] = @user.id
+      redirect "/"
+  else
+    #put an alert here
+    redirect "/login"
+  end
+
+end
