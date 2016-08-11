5fb1606a849b7181fa9ad38aae9a677a8e762271
Katrina Tollin & Jessica Bohn & Jason Pelletier <pair+ATXkatrina.JessBohn.lukin41280@devbootcamp.com>
2016-08-10 22:15:45 -0500

    Fix typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ tom = User.create(username: "tomscode", password: "istherealjoke")
 
 
 post1 = Post.create(title: "Drunk AF - need snack", question: "I just drank 17 beers (not that light shit either) and I really need a fucking snack. What would you suggest?")
-post2 = Post.create(title: "Recent breakup - help", question: "My boyfriend just dumped for a set of twins. I am currently weeping in Wal-Mart, what snacks should I buy to drown my sorrows")
+post2 = Post.create(title: "Recent breakup - help", question: "My boyfriend just dumped me for a set of twins. I am currently weeping in Wal-Mart, what snacks should I buy to drown my sorrows")
 post3 = Post.create(title: "New to America", question: "I new to here. I like the snack but I am different for what to eat. Americans, give me word for a snack")
 post4 = Post.create(title: "Party up in here!", question: "I am amidst a killer rager and we are out of snacks!!!! FML!!! I need some quick ideas to stock up party")
 post5 = Post.create(title: "Yo Dawg", question: "I need some ideas on how to pimp out my snacks")
