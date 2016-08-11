738f9383b3b6df980598c470995379fe2b159bcb
Jason Pelletier <jpelletier19@me.com>
2016-08-11 11:54:07 -0500

    can vote. now refactoring to include downvotes and applicable resources

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,4 +1,14 @@
-post "/votes" do
+post "/votes/up" do
+  resource = Post.find(params[:_method].to_i)
 
-  # vote = Vote.create(upvote: )
+  Vote.create(upvote: true, user_id: session[:id], votable_id: resource.id, votable_type: resource.class)
+  redirect "/"
+
 end
+
+post "/votes/down" do
+
+
+end
+
+
