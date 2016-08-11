b1655205d999e97bbf55ee1b492800316be04589
Jason Pelletier <jpelletier19@me.com>
2016-08-11 12:24:26 -0500

    made method to count every vote for user's comments, answers, and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :answers
   has_many :posts
+
+  def count_all_votes
+    self.comments.votes.count + self.answer.votes.count + self.posts.votes.count
+  end
 end
