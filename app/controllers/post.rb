1fb27cabcb3aae80a69c2f8e93794286d6e92a31
Katrina Tollin & Jessica Bohn & Jason Pelletier <pair+ATXkatrina.JessBohn.lukin41280@devbootcamp.com>
2016-08-11 14:45:47 -0500

    Ability to answers other users questions

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -8,16 +8,30 @@ post '/post/new' do
   @post = Post.new(title: params[:post_title], question: params[:question])
   if @post.save && @user
     @user.posts << @post
-    redirect "/post/#{@post.id}/show"
+    redirect "/post/#{@post.id}"
   else
     redirect '/login'
   end
 
 end
 
-get '/post/:id/show' do
+get '/post/:id' do
+  p params
   @post = Post.find(params[:id])
   @author = User.find(@post.user_id).username
 
   erb :"posts/show"
+end
+
+post '/post/:id' do
+  @user = User.find(session[:id])
+  @post = Post.find(params[:id])
+  @answer = Answer.new(description: params[:your_answer])
+  if @user && @answer.save
+    @user.answers << @answer
+    @post.answers << @answer
+    redirect "/post/#{@post.id}"
+  else
+    redirect '/login'
+  end
 end
