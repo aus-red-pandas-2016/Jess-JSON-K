984c011dcd096e879d01e204405c9939783786b7
Katrina Tollin <email": null>
2016-08-11 22:22:02 -0500

    Get commenting to good place

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,15 +1,14 @@
 get '/comment/new' do
-  # @lineage = params[:resource]
   erb :"comment/new"
 end
 
 post '/comment/new' do
   @user = User.find(current_user)
   @post = Post.find(params[:post_id])
-  @answer = Answer.find(params[:resource_id])
 
   case params[:resource_class]
   when "Answer"
+      @answer = Answer.find(params[:resource_id])
       Comment.create(description: params[:comment], user_id: @user.id, commentable_id: @answer.id, commentable_type: @answer.class.name)
 
   when "Post"
@@ -19,7 +18,7 @@ post '/comment/new' do
   if request.xhr?
     erb(:"comment/show", layout: false)
   else
-    redirect "/post/#{@post.id}/show"
+    redirect "/post/#{@post.id}"
   end
 end
 
